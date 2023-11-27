ac612f8b231cb2953e56ef75352fa3934415552e
Linhholi <103567688+Linhholi@users.noreply.github.com>
2023-11-27 18:13:41 +1100

    responsive

---
+++ src/components/skill.jsx
@@ -0,0 +1,77 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { SkillsCard } from "./skillcard";
+import "./skill.css";
+
+export const SkillsSection = ({className, id }) => {
+  return (
+    <div className={`skills-section ${className}`} id="skills">
+      <div className="text-wrapper">SKILL SET</div>
+      <div className="cards-section">
+        <SkillsCard
+          className="skills-card-instance"
+          description={
+            <>
+              Programming languages: Python, SQL
+              <br />
+              Statistical Analysis:&nbsp;&nbsp;hypothesis testing, regression, clustering, etc
+              <br />
+              Machine Learning and AI:&nbsp;&nbsp;algorithms like decision trees, neural networks, SVMs, and deep
+              learning frameworks (TensorFlow, Yolo)
+            </>
+          }
+          title="Data Science"
+          version="one"
+        />
+        <SkillsCard
+          className="skills-card-instance"
+          description={
+            <>
+              Data Manipulation and Cleaning: Experience with data cleaning, preprocessing, and manipulation using
+              Pandas, NumPy
+              <br />
+              Data Visualisation: Matplotlib, Seaborn, Tableau
+            </>
+          }
+          title="Data Analysis"
+          version="two"
+        />
+      </div>
+      <div className="cards-section">
+      {/* <div className="div"> */}
+        <SkillsCard
+          className="skills-card-instance"
+          // className="skills-card-5"
+          description={
+            <>
+              Industry Knowledge: Understanding of the specific industry or field (finance, healthcare, automotive)
+              <br />
+              Business Acumen: Ability to translate data findings into actionable insights that benefit the business
+            </>
+          }
+          title="Business Analysis"
+          version="three"
+        />
+        <SkillsCard
+          className="skills-card-instance"
+          // className="skills-card-5"
+          description={
+            <>
+              Project Management: Skills in organizing, planning, and executing projects, setting timelines, and
+              ensuring successful outcomes.
+              <br />
+              Decision-Making: Capable of making informed and data-driven decisions, even in ambiguous or complex
+              situations.
+            </>
+          }
+          title="Leadership"
+          version="four"
+        />
+      </div>
+    </div>
+  );
+};
+
+SkillsSection.propTypes = {
+  showButton: PropTypes.bool
+};
---
+++ src/components/skillcard.jsx
@@ -0,0 +1,54 @@
+import PropTypes from "prop-types";
+import React from "react";
+import "./skillcard.css";
+import datascience from "../images/datascience.png"
+import dataana from "../images/dataana.jpeg"
+import business from "../images/business-analyst.jpeg"
+import leader from "../images/leader.jpeg"
+
+export const SkillsCard = ({
+  description="",
+  title = "Title",
+  property1,
+  className,
+  version
+}) => {
+  let imageSource;
+
+  // Choose the image source based on the version prop
+  switch (version) {
+    case "one":
+      imageSource = datascience; 
+      break;
+    case "two":
+      imageSource = dataana;
+      break;
+    case "three":
+        imageSource = business; 
+        break;
+    default:
+        imageSource = leader; 
+    }
+  return (
+    <div className={`skills-card ${className}`}>
+        <div className="skills-card-icon">
+          <img
+              className="skills-card-icon-instance"
+              src={imageSource}
+              alt="Skill icon"
+            />
+        </div>
+          <div className="div">
+            <div className="text-wrapper">{title}</div>
+            <p className="p">{description}</p>
+          </div>
+    </div>
+  );
+};
+
+SkillsCard.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string,
+  image: PropTypes.string,
+  version: PropTypes.oneOf(["one", "two","three","four"]),
+};
